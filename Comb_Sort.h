8501472d434e9f788d03380c1d9f5ccd27cef60e
nesquik-sd <146976913+nesquik-sd@users.noreply.github.com>
2023-10-05 17:09:24 +0300

    Delete Comb_Sort.h

--- Comb_Sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Check_Func.h"
-#include "Work_With_Files.h"
-#include "iostream"
-using namespace std; 
-
-double* comb_sort(double* arr, int quanity); //реализация сортировки
-void unit_test(); //модульное тестирование (автоматическое заполенение с файла)
-
